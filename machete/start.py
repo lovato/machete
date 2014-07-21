d8e45e5fdcbcea22b155a19b5575388329d2af50
lovato <maglovato@gmail.com>
2014-07-21 13:39:16 -0300

    restrict options to available templates

--- machete/start.py
+++ machete/start.py
@@ -5,15 +5,24 @@
 from machete import __version__, log
 from machete.submodule import module
 import argparse
-
+import os
 
 def main():
     log.info("machete v" + __version__)
     parser = argparse.ArgumentParser(description='machete v' + __version__)
 
+    path = os.path.abspath(os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "templates"))
+    templates = [fn for fn in os.listdir(path) if any([not fn.startswith('_')])];
+
+    for each in templates:
+        print each
+
     parser.add_argument(
         '-t', '--template',
-        help='Select one of the available templates (TODO list them here).',
+        help='Select one of the available templates. Allowed values are '+', '.join(templates),
+        metavar='<template_name>',
+        choices=templates, 
         required=True)
     parser.add_argument(
         "--chicken", help="Chicken mode (optional). Does NOT CHANGE anything.",
