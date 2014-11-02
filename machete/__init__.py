a316904ba0040dd57b418a226dcb09ff431bea39
Marco Lovato <maglovato@gmail.com>
2014-11-02 02:18:19 -0200

    travis fixes

--- machete/__init__.py
+++ machete/__init__.py
@@ -27,6 +27,14 @@ log.basicConfig(level=logging.DEBUG,
 def __path(filename):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)
 
+if os.getenv("TRAVIS_BUILD_NUMBER"):
+	print os.getenv("TRAVIS_BUILD_NUMBER")
+    file_ = open('build.info', 'w')
+    file_.write(os.getenv("TRAVIS_BUILD_NUMBER"))
+    file_.close()
+else:
+	print 22222222
+
 if os.path.exists(__path('build.info')):
     __build__ = open(__path('build.info')).read().strip()
 else:
